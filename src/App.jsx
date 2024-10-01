16a6b14743f8ff72d7830fcb3c9902137566284d
nithinnaidubabu <nithinnaidubabu@gmail.com>
2024-10-01 14:55:33 -0400

    [Feat]: Added resume download feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Resume, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
@@ -11,10 +11,10 @@ const App = () => {
           <Hero />
         </div>
         <About />
-        <Experience />
+        
         <Tech />
         <Works />
-        <Feedbacks />
+        <Resume />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
